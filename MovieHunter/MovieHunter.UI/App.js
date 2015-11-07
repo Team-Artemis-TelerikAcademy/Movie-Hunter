78074f6c91465e8ea2d27ebe6e69eb94518a98e0
emilti <emilti1986@gmail.com>
2015-11-07 18:06:29 +0200

    Initial files for UI part

---
+++ MovieHunter/MovieHunter.UI/App.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    var sammyApp = Sammy('#content', function () {
+        this.get('#/', function () {
+            this.redirect('#/home');
+        });
+        this.get('#/home', homeController.all);
+    });
+
+    $(function () {
+        sammyApp.run('#/');
+    });
+}());
---
+++ MovieHunter/MovieHunter.UI/App/templates.js
@@ -0,0 +1,25 @@
+﻿var templates = function () {
+    var handlebars = window.handlebars || window.Handlebars,
+        Handlebars = window.handlebars || window.Handlebars,
+        cache = {};
+
+    function get(name) {
+        var promise = new Promise(function (resolve, reject) {
+            if (cache[name]) {
+                resolve(cache[name]);
+                return;
+            }
+            var url = `Templates/${name}.handlebars`;
+            $.get(url, function (html) {
+                var template = handlebars.compile(html);
+                cache[name] = template;
+                resolve(template);
+            });
+        });
+        return promise;
+    }
+
+    return {
+        get: get
+    };
+}();
