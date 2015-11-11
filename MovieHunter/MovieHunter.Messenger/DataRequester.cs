51a4075e79af7352e0d1ee9965a721767e646971
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-11-11 21:30:31 +0200

    DataRequester for desktop client

---
+++ MovieHunter/MovieHunter.Messenger/DataRequester.cs
@@ -0,0 +1,43 @@
+﻿namespace ChatTest
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+    using FLExtensions.Common;
+
+    public class DataRequester
+    {
+        public string Get(string url, ICollection<string> headers = null)
+        {
+            if(headers == null)
+            {
+
+            }
+        }
+
+        public string Request(string url, ICollection<string> headers = null)
+        {
+            var request = WebRequest.CreateHttp(url);
+
+            if(headers != null && headers.Count > 0)
+            {
+                headers.ForEach(h => request.Headers.Add(h));
+            }
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var receiveStream = response.GetResponseStream();
+
+            var readStream = new StreamReader(receiveStream, Encoding.UTF8);
+            var json = readStream.ReadToEnd();
+
+            receiveStream.Close();
+            readStream.Close();
+
+            return json;
+        }
+    }
+}
