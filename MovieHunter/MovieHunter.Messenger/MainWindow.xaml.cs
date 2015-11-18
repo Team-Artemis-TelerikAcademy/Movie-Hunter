ee53c4e699fb7fa1e48114b6f35e86132c826ba5
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-11-19 01:45:28 +0200

    Merge update from master

--- MovieHunter/MovieHunter.Messenger/MainWindow.xaml.cs
+++ MovieHunter/MovieHunter.Messenger/MainWindow.xaml.cs
@@ -4,22 +4,14 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
     using System.Windows.Input;
     using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
     using Newtonsoft.Json;
     using FLExtensions.Common;
     using Common.PubNubMessaging.Core;
-    using System.Windows.Markup;
-    using System.Xml;
     using System.Text.RegularExpressions;
 
     /// <summary>
