d24d31c62ef3ee1c3a7fdeff183b335f080b3f1f
emilti <emilti1986@gmail.com>
2015-11-19 02:07:08 +0200

    Added form for changing the rating

--- MovieHunter/MovieHunter.UI/Controllers/movie-controller.js
+++ MovieHunter/MovieHunter.UI/Controllers/movie-controller.js
@@ -17,6 +17,7 @@ var movieController = function () {
         jsonRequester.get('http://localhost:52189/api/Movies/' + id)
              .then(function (res) {
                  movie = res;
+                console.log(movie);
                  return templates.get('movieById');
              }).then(function (template) {
                  context.$element().html(template(movie));
@@ -70,6 +71,42 @@ var movieController = function () {
 
                      $(this).attr('href', editedAttribute);
                  })
+
+
+
+                $('#chanceSlider').on('change', function(){
+                    $('#chance').val($('#chanceSlider').val());
+                });
+
+                $('#chance').on('keyup', function(){
+                    $('#chanceSlider').val($('#chance').val());
+                });
+
+                $('.change-rating').on('click', function(){
+                    $('.slider-form').css('display', 'inline-block');
+                    $('.change-rating').css('display', 'none')
+                })
+
+
+                $('.submit-new-rating').on('click', function(){
+                    var newMovieRatingModel =
+                    {
+                        movieId: movie.Id,
+                        Rating: $('#chance').val()
+                    };
+
+                    var newMovieRatingModelStringified = JSON.stringify(newMovieRatingModel);
+                    var authorization = "Bearer " + localStorage.getItem("tokenKey");
+
+                    jsonRequester.put('http://localhost:52189/api/my-movies/rating', { data: newMovieRatingModelStringified, contentType: 'application/json', headers: { Authorization: authorization } })
+                        .then(function () {
+                            toastr.success('Rating Successfully Changed');
+                            $('.slider-form').css('display','none');
+                            $('.change-rating').css('display', 'inline-block');
+                        })
+                })
+
+
              })
     }
 
@@ -115,23 +152,4 @@ var movieController = function () {
         released: released,
         comingSoon: comingSoon
     };
-
-
-
-
-
-    //         jsonRequester.get('http://localhost:52189/api/Movies:'+this.params.id)
-    //             .then(function(res) {
-    //                 item = res.result;
-    //                 return templates.get('item-details');
-    //             })
-    //             .then(function(html) {
-    //                 var template = handlebars.compile(html);
-    //                 $('#content').html(template(item));
-    //             });
-    //     });
-    //     return {
-    //         all: all
-    //     };
 }();
-//
