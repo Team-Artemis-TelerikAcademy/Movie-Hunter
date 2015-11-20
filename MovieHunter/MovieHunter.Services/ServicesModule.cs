67ecd6be765bceba6ecde3fb01e6f5b44388bca4
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-11-20 08:54:06 +0200

    Fixed .csproj error from merge

--- MovieHunter/MovieHunter.Services/ServicesModule.cs
+++ MovieHunter/MovieHunter.Services/ServicesModule.cs
@@ -9,8 +9,6 @@
     {
         public void RegisterBindings(IKernel kernel)
         {
-            
-
             kernel.Bind<IActorsService>().ToMethod(c => new ActorsService(kernel.Get<IRepository<Actor>>()));
             kernel.Bind<IGenresService>().ToMethod(c => new GenresService(kernel.Get<IRepository<Genre>>()));
             kernel.Bind<IMoviesService>().ToMethod(c => new MoviesService(kernel.Get<IRepository<Movie>>()));
