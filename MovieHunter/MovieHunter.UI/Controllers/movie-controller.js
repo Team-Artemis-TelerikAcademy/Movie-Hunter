5be0c3e761446f3023edb933d6b673532c252711
Margarita Ivancheva <mvivancheva@gmail.com>
2015-11-20 11:49:45 +0200

    upload

--- MovieHunter/MovieHunter.UI/Controllers/movie-controller.js
+++ MovieHunter/MovieHunter.UI/Controllers/movie-controller.js
@@ -99,7 +99,8 @@ var movieController = function () {
                     jsonRequester.get('http://moviehunterproject.azurewebsites.net/api/movies/' + likedMovie.Id + '/download-wallpaper', { contentType: 'application/json'})
                          .then(function (resp) {
                              url = resp;
-                             $('#link-to-dropbox').attr("href", "url")
+                             console.log(url);
+                             $('#link-to-dropbox').attr("href", "http://www.google.com/")
                              $('#link-to-dropbox').css('display', 'inline-block');
                          })
                 });
